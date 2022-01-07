fc11642a7915d858122675790d60df5dfd7916fe
RealThadeous <bor21004@byui.edu>
2022-01-07 15:27:17 +0200

    week 1 update to portfolio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const items = [
     {
-        label: "Week 1 Notes",
-        url: "notes/wk1_notes.txt",
+        label: "WEEK 1",
+        url: "week1/index.html",
     }
     
 ]
+
+items.forEach((item) => {
+    let point = document.createElement("li");    
+    let week = document.createElement("a");
+
+    week.innerHTML = item.label;
+    week.href = item.url;
+
+    point.appendChild(week);
+    document.querySelector("ol.list").appendChild(point);
+});
